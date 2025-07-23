e64c3c2996df01a2bc23c5d474e61338fdafb4fa
Wilker Martins <dev.wilker@gmail.com>
2025-07-23 20:29:56 -0300

    as

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Se você tiver alguma configuração customizada (ex: para imagens),
+  // ela deve vir aqui dentro. Por exemplo:
+  // images: {
+  //   remotePatterns: [],
+  // },
+};
+
+export default nextConfig;
